a268a57276db56b0be8ecaf41c90ca21295f25a3
Wilton Rodrigues <wilton.rodrigues@4linux.com.br>
2021-09-28 15:24:12 -0300

    Creating plugin base structure

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/wiltonsr/ldapauth
 
-go 1.16
+go 1.17
+
+require (
+	github.com/abbot/go-http-auth v0.4.0
+	github.com/go-ldap/ldap/v3 v3.4.1
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20200615164410-66371956d46c // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
+)
--- ldapauth.go
+++ ldapauth.go
@@ -1,66 +1,115 @@
-// Package ldapauth a ldap authentication plugin.
+// Package LdapAuth a ldap authentication plugin.
 package ldapauth
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"text/template"
+	"net/url"
+
+
+	"github.com/go-ldap/ldap/v3"
+)
+
+const (
+	defaultRealm        = "traefik"
+	authorizationHeader = "Authorization"
+	contentType         = "Content-Type"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
+	Enabled      bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Debug        bool   `json:"debug,omitempty" yaml:"debug,omitempty"`
+	Host         string `json:"host,omitempty" yaml:"host,omitempty"`
+	Port         uint16 `json:"port,omitempty" yaml:"port,omitempty"`
+	BaseDn       string `json:"baseDn,omitempty" yaml:"baseDn,omitempty"`
+	UserUniqueId string `json:"useruniqueid,omitempty" yaml:"useruniqueid,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		Headers: make(map[string]string),
+		Enabled:        true,
+		Debug:          false,
+		Host:           "example.com",
+		Port:           389,
+		BaseDn:         "dc=example,dc=org",
+		UserUniqueId:   "uid", // Usually uid or sAMAccountname
 	}
 }
 
-// Demo a Demo plugin.
-type Demo struct {
+// LdapAuth Struct plugin.
+type LdapAuth struct {
 	next     http.Handler
-	headers  map[string]string
 	name     string
-	template *template.Template
+	config   *Config
 }
 
-// New created a new Demo plugin.
+// New created a new LdapAuth plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
+	log.Println("Starting", name ,"Middleware...")
+	if config.Debug {
+		log.Println("Enabled       =>", config.Enabled)
+		log.Println("Host          =>", config.Host)
+		log.Println("Port          =>", config.Port)
+		log.Println("BaseDn        =>", config.BaseDn)
+		log.Println("UserUniqueId  =>", config.UserUniqueId)
 	}
 
-	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+	return &LdapAuth{
+		name:   name,
+		next:   next,
+		config: config,
 	}, nil
 }
 
-func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func (la *LdapAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	
+	if !la.config.Enabled {
+		log.Printf("%s Disabled! Passing request...", la.name)
+		la.next.ServeHTTP(rw, req)
+		return
+	}
+	
+	user, password, ok := req.BasicAuth()
 
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
+	if !ok {
+        // No valid 'Authentication: Basic xxxx' header found in request
+		la.RequireAuth(rw, req)
+		return
 	}
 
-	a.next.ServeHTTP(rw, req)
+	isValidUser := la.ldapCheckUser(user, password)
+
+	if !isValidUser {
+		log.Printf("Authentication failed")
+		la.RequireAuth(rw, req)
+		return
+	} else {
+		log.Printf("Authentication succeeded")
+	}
+
+	// Sanitize Some Headers Infos
+	req.URL.User = url.User(user)
+	req.Header["LDAP-User"] = []string{user}
+	req.Header.Del("Authorization")
+
+	la.next.ServeHTTP(rw, req)
+}
+
+func (la *LdapAuth) ldapCheckUser(user, password string) bool {
+	log.Printf("user=%s password=%s", user, password)
+	if user == "john" {
+		return true
+	}
+	return false
+}
+
+func (la *LdapAuth) RequireAuth(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set(contentType, "text/plan")
+	w.Header().Set("WWW-Authenticate", `Basic realm="`+defaultRealm+`"`)
+	w.WriteHeader(401)
+	w.Write([]byte(fmt.Sprintf("%d %s bbb\n", http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))))
 }
