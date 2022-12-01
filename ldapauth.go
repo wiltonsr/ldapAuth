34bda656d31dcefe8b4ddbc4981a4c37c594911e
Wilton Rodrigues <wiltonsr94@gmail.com>
2022-12-01 17:08:28 -0300

    Prevent remove spaces in searchFilter
    - Fixes #29

--- ldapauth.go
+++ ldapauth.go
@@ -439,7 +439,6 @@ func ParseSearchFilter(config *Config) (string, error) {
 
 	filter = strings.Trim(filter, "\n\t")
 	filter = strings.TrimSpace(filter)
-	filter = strings.ReplaceAll(filter, " ", "")
 	filter = strings.Replace(filter, "\\", "", -1)
 
 	tmpl, err := template.New("search_template").Parse(filter)
